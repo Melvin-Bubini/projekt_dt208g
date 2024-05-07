33e42b82c8cdb916f69890e11c6204e856b2e585
Melvin Bubini <melvin.bubini@gmail.com>
2024-05-07 15:28:21 +0200

    funktionalitet för kurser klar förutom lägg till i ramschema

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -12,13 +12,71 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
-  coursedatalist: Coursedata[] = [];
 
-  constructor(private coursedataservice : CoursedataService) {}
+  // Properties
+  coursedatalist: Coursedata[] = [];
+  filteredCoursedata: Coursedata[] = [];
+  filterValue: string = "";
+  sortedCoursedata: Coursedata[] = [];
+  sortedValue: string = "";
+  uniqueSubjects: string[] = []; // Lägg till en egenskap för att lagra unika ämnen
+  selectedSubject: string = ""; // Lägg till en egenskap för att lagra det valda ämnet
+
+  constructor(private coursedataservice: CoursedataService) { }
 
   ngOnInit() {
     this.coursedataservice.getCoursedata().subscribe(data => {
       this.coursedatalist = data;
-    })
+      this.filteredCoursedata = data;
+      this.generateUniqueSubjects(); // Generera en lista över unika ämnen när data laddas
+    });
   }
+
+  filterCoursedata(): void {
+    this.filteredCoursedata = this.coursedatalist.filter((coursedata) =>
+      coursedata.courseCode.toLowerCase().includes(this.filterValue.toLowerCase()) ||
+      coursedata.courseName.toLowerCase().includes(this.filterValue.toLowerCase())
+    );
+  }
+
+  sortingFilter(sortBy: string): void {
+    switch (sortBy) {
+      case 'kurskod':
+        this.filteredCoursedata = this.coursedatalist.slice().sort((a, b) => a.courseCode.localeCompare(b.courseCode));
+        break;
+
+      case 'kursnamn':
+        this.filteredCoursedata = this.coursedatalist.slice().sort((a, b) => a.courseName.localeCompare(b.courseName));
+        break;
+
+      case 'points':
+        this.filteredCoursedata = this.coursedatalist.slice().sort((a, b) => b.points - a.points);
+        break;
+
+      case 'subject':
+        this.filteredCoursedata = this.coursedatalist.slice().sort((a, b) => a.subject.localeCompare(b.subject));
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  // Metod för att generera en lista över unika ämnen från kursdatalistan
+  generateUniqueSubjects(): void {
+    this.uniqueSubjects = [...new Set(this.coursedatalist.map(coursedata => coursedata.subject))];
+  }
+
+  // Metod för att filtrera data efter det valda ämnet
+  filterBySubject(): void {
+    if (this.selectedSubject) {
+      this.filteredCoursedata = this.coursedatalist.filter(coursedata => coursedata.subject === this.selectedSubject);
+    } else {
+      this.filteredCoursedata = this.coursedatalist; // Visa alla kurser om inget ämne är valt
+    }
+  }
+
+
 }
+
+
